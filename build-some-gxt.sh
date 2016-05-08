09ef007973bf0bcdd98043c6c38ccd8943ac0aa2
C. Titus Brown <titus@idyll.org>
2016-05-08 12:59:05 +0000

    another example

--- build-some-gxt.sh
+++ build-some-gxt.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-./spacegraphcats/walk-dbg.py data/tr-cross.fa -o tr-cross.gxt
-./spacegraphcats/walk-dbg.py data/tr-cross.fa -o tr-cross.gml --gml
+set -x
+set -e
+./spacegraphcats/walk-dbg.py -x 5e7 data/tr-cross.fa -o tr-cross.gxt
+./spacegraphcats/walk-dbg.py -x 5e7 data/tr-cross.fa -o tr-cross.gml --gml
+./spacegraphcats/walk-dbg.py -x 1e8 data/acido-short.fa -o acido-short.gxt
