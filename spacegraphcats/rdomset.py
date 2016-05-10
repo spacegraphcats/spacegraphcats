0aa9d1216c4f50bee91e07c31641bdcaa2b3afbd
Felix Reidl <felix.reidl@gmail.com>
2016-05-10 13:57:07 -0400

    Fixed argparse help

--- spacegraphcats/rdomset.py
+++ spacegraphcats/rdomset.py
@@ -301,11 +301,11 @@ def main(inputgxt, outputgxt, d, test, domgraph):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('input', help='gml input file', nargs='?', type=argparse.FileType('r'),
+    parser.add_argument('input', help='gxt input file', nargs='?', type=argparse.FileType('r'),
                         default=sys.stdin)
-    parser.add_argument('output', help='gml output file', nargs='?', type=argparse.FileType('w'),
+    parser.add_argument('output', help='gxt output file', nargs='?', type=argparse.FileType('w'),
                         default=sys.stdin)
-    parser.add_argument('--domgraph', help='gml output file', nargs='?', type=argparse.FileType('w'),
+    parser.add_argument('--domgraph', help='gxt output file', nargs='?', type=argparse.FileType('w'),
                         default=None)
     parser.add_argument("d", help="domset distance", type=int )
     parser.add_argument('--test', action='store_true')
