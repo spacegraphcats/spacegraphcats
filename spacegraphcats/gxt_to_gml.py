f9744afd976658910fea2d0b88570d29a1849754
Felix Reidl <felix.reidl@gmail.com>
2016-05-12 16:30:46 -0400

    Gxt to gml converted

---
+++ spacegraphcats/gxt_to_gml.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from parser import parse, GmlWriter
+import sys
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='gxt input file', nargs='?',
+                        type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('output', help='gml output file', nargs='?',
+                        type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('--directed', help='set flag if graph is supposed to be directed', action='store_true', default=False)
+    args = parser.parse_args()
+
+    writer = GmlWriter(args.output, directed=args.directed)
+
+    def add_vertex(v, size, attributes, values):
+            writer.add_vertex(v, size, values, attributes)
+
+    def add_edge(u, v, attributes, values):
+        writer.add_edge(u, v, values, attributes)
+
+    parse(args.input, add_vertex, add_edge)
+    writer.done()
