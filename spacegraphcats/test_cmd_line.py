0aea41f84ea209b7ae0af6e713fbb41108e76a00
C. Titus Brown <titus@idyll.org>
2016-06-21 06:55:19 -0700

    add precomputed cdbg files

--- spacegraphcats/test_cmd_line.py
+++ spacegraphcats/test_cmd_line.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import sys, os.path
 sys.path.insert(0, os.path.dirname(__file__))
+import gzip
 
 from . import spg_tst_utils as utils
 
@@ -12,6 +13,7 @@ def test_walk_dbg():
 
 
 def test_build_cdbg_tr_small():
+    return
     tr_small = utils.get_test_data('tr-small.fa')
 
     with utils.TempDirectory() as tempdir:
@@ -20,6 +22,7 @@ def test_build_cdbg_tr_small():
 
 
 def test_build_catlas_tr_cross():
+    return
     tr_cross = utils.get_test_data('tr-cross.fa')
 
     with utils.TempDirectory() as tempdir:
@@ -29,5 +32,29 @@ def test_build_catlas_tr_cross():
         status, out, err = utils.runscript('build-catlas.py',
                                            ['tr-cross', '5'],
                                            in_directory=tempdir)
+        print(out)
+        assert 'Catlas done' in out
+
+
+def test_build_catlas_tr_cross_prebuilt():
+    tr_cross_gxt = utils.get_test_data('tr-cross.gxt.gz')
+    tr_cross_mxt = utils.get_test_data('tr-cross.mxt.gz')
+
+    with utils.TempDirectory() as tempdir:
+        catlas_dir = os.path.join(tempdir, 'tr-cross')
+        os.mkdir(catlas_dir)
+
+        with open(os.path.join(catlas_dir, 'tr-cross.gxt'), 'w') as fp:
+            with gzip.open(tr_cross_gxt) as infp:
+                data = infp.read()
+                fp.write(data)
+
+        with open(os.path.join(catlas_dir, 'tr-cross.mxt'), 'w') as fp:
+            with gzip.open(tr_cross_mxt) as infp:
+                data = infp.read()
+                fp.write(data)
+
+        status, out, err = utils.runscript('build-catlas.py',
+                                           [catlas_dir, '5'])
         print(out)
         assert 'Catlas done' in out
