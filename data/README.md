1b47ce7ac638186f4e497728ffef4309af50eb8d
C. Titus Brown <titus@idyll.org>
2016-06-21 07:33:55 -0700

    add a bunch of tests for catlas and mh

---
+++ data/README.md
@@ -0,0 +1,16 @@
+To compute
+
+```
+acido-short.fa.sig.dump.txt
+tr-2.fa.sig.dump.txt
+tr-1.fa.sig.dump.txt
+```
+
+execute:
+
+```
+sourmash compute tr-1.fa tr-2.fa acido-short.fa
+sourmash dump tr-1.fa.sig 
+sourmash dump tr-2.fa.sig 
+sourmash dump acido-short.fa.sig 
+```
--- spacegraphcats/test_cmd_line.py
+++ spacegraphcats/test_cmd_line.py
@@ -2,8 +2,23 @@
 import sys, os.path
 sys.path.insert(0, os.path.dirname(__file__))
 import gzip
+import shutil
 
 from . import spg_tst_utils as utils
+
+
+def test_gxt_to_gml():
+    # can we run gxt_to_gml?
+    tr_cross_gxt = utils.get_test_data('tr-cross.gxt.gz')
+
+    with utils.TempDirectory() as tempdir:
+        with open(os.path.join(tempdir, 'tr-cross.gxt'), 'wb') as fp:
+            with gzip.open(tr_cross_gxt, 'rb') as infp:
+                data = infp.read()
+                fp.write(data)
+
+        utils.runscript('gxt_to_gml.py', ['tr-cross.gxt', 'out.gml'],
+                        in_directory=tempdir)
 
 
 def test_walk_dbg():
@@ -13,6 +28,7 @@ def test_walk_dbg():
 
 
 def test_build_cdbg_tr_small():
+    # check a basic run of walk-dbg
     tr_small = utils.get_test_data('tr-small.fa')
 
     with utils.TempDirectory() as tempdir:
@@ -21,6 +37,7 @@ def test_build_cdbg_tr_small():
 
 
 def test_build_catlas_tr_cross():
+    # can we build a catlas from the output of walk-dbg?
     tr_cross = utils.get_test_data('tr-cross.fa')
 
     with utils.TempDirectory() as tempdir:
@@ -34,25 +51,97 @@ def test_build_catlas_tr_cross():
         assert 'Catlas done' in out
 
 
+def _build_catlas_from_gz(gxtfile, mxtfile, tempdir, level=5):
+    # construct catlas name from first component of gxtfile
+    catlas_name = os.path.basename(gxtfile).split('.')[0]
+    catlas_dir = os.path.join(tempdir, catlas_name)
+    os.mkdir(catlas_dir)
+
+    # copy over the gz gxt file
+    with open(os.path.join(catlas_dir, catlas_name + '.gxt'), 'wb') as fp:
+        with gzip.open(gxtfile, 'rb') as infp:
+            data = infp.read()
+            fp.write(data)
+
+    # copy over the gz mxt file
+    with open(os.path.join(catlas_dir, catlas_name + '.mxt'), 'wb') as fp:
+        with gzip.open(mxtfile, 'rb') as infp:
+            data = infp.read()
+            fp.write(data)
+
+    # run build-catlas!
+    status, out, err = utils.runscript('build-catlas.py',
+                                       [catlas_dir, str(level)])
+    print(out)
+    # make sure it worked...
+    assert 'Catlas done' in out
+
+    return catlas_dir
+
+
 def test_build_catlas_tr_cross_prebuilt():
+    # can we build a catlas from prebuilt gxt/mxt?
     tr_cross_gxt = utils.get_test_data('tr-cross.gxt.gz')
     tr_cross_mxt = utils.get_test_data('tr-cross.mxt.gz')
 
     with utils.TempDirectory() as tempdir:
-        catlas_dir = os.path.join(tempdir, 'tr-cross')
-        os.mkdir(catlas_dir)
+        dirname = _build_catlas_from_gz(tr_cross_gxt, tr_cross_mxt,
+                                        tempdir)
 
-        with open(os.path.join(catlas_dir, 'tr-cross.gxt'), 'wb') as fp:
-            with gzip.open(tr_cross_gxt, 'rb') as infp:
-                data = infp.read()
-                fp.write(data)
 
-        with open(os.path.join(catlas_dir, 'tr-cross.mxt'), 'wb') as fp:
-            with gzip.open(tr_cross_mxt, 'rb') as infp:
-                data = infp.read()
-                fp.write(data)
+def test_search_new_catlas():
+    # build a catlas and then search it --
+    tr_cross_gxt = utils.get_test_data('tr-cross.gxt.gz')
+    tr_cross_mxt = utils.get_test_data('tr-cross.mxt.gz')
 
-        status, out, err = utils.runscript('build-catlas.py',
-                                           [catlas_dir, '5'])
+    mh1_txt = utils.get_test_data('tr-1.fa.sig.dump.txt')
+    mh2_txt = utils.get_test_data('tr-2.fa.sig.dump.txt')
+    mh3_txt = utils.get_test_data('acido-short.fa.sig.dump.txt')
+
+    with utils.TempDirectory() as tempdir:
+        dirname = _build_catlas_from_gz(tr_cross_gxt, tr_cross_mxt,
+                                        tempdir)
+
+        status, out, err = utils.runscript('search-catlas-mh.py',
+                            [dirname, mh1_txt], in_directory=tempdir)
+        assert 'tr-1.fa.sig.dump.txt,0.488,0.976,0,1' in out
+
+        status, out, err = utils.runscript('search-catlas-mh.py',
+                            [dirname, mh2_txt], in_directory=tempdir)
+        assert 'tr-2.fa.sig.dump.txt,0.500,1.000,0,1' in out
+
+        status, out, err = utils.runscript('search-catlas-mh.py',
+                            [dirname, mh3_txt], in_directory=tempdir)
+
+        assert out.endswith('levels\n---\n'), (out,)
         print(out)
-        assert 'Catlas done' in out
+
+
+def test_search_prebuilt_catlas():
+    # use a pre-built catlas and search it --
+    tr_cross_dir = utils.get_test_data('catlas.tr-cross')
+
+    mh1_txt = utils.get_test_data('tr-1.fa.sig.dump.txt')
+    mh2_txt = utils.get_test_data('tr-2.fa.sig.dump.txt')
+    mh3_txt = utils.get_test_data('acido-short.fa.sig.dump.txt')
+
+    with utils.TempDirectory() as tempdir:
+        dirname = os.path.join(tempdir, 'tr-cross')
+        shutil.copytree(tr_cross_dir, dirname)
+
+        # results
+        status, out, err = utils.runscript('search-catlas-mh.py',
+                            [dirname, mh1_txt], in_directory=tempdir)
+        assert 'tr-1.fa.sig.dump.txt,0.488,0.976,0,1' in out
+
+        # results
+        status, out, err = utils.runscript('search-catlas-mh.py',
+                            [dirname, mh2_txt], in_directory=tempdir)
+        assert 'tr-2.fa.sig.dump.txt,0.500,1.000,0,1' in out
+
+        # no results
+        status, out, err = utils.runscript('search-catlas-mh.py',
+                            [dirname, mh3_txt], in_directory=tempdir)
+
+        assert out.endswith('levels\n---\n'), (out,)
+        print(out)
