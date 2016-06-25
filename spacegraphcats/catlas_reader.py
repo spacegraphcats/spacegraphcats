64d50f9a64015a6c5bf39a0e187b065c0b9df601
C. Titus Brown <titus@idyll.org>
2016-06-25 06:29:20 -0700

    allow for fully-qualified paths as catlas prefix

--- spacegraphcats/catlas_reader.py
+++ spacegraphcats/catlas_reader.py
@@ -34,19 +34,20 @@ class CAtlasReader(object):
             return set([self.leaves_to_domnode[node_id]])
 
     def _load(self):
-        catlas_gxt = '%s.catlas.%d.gxt' % (self.prefix, self.radius)
+        basename = os.path.basename(self.prefix)
+        catlas_gxt = '%s.catlas.%d.gxt' % (basename, self.radius)
         catlas_gxt = os.path.join(self.prefix, catlas_gxt)
         self.catlas_gxt = catlas_gxt
 
-        assignment_vxt = '%s.assignment.%d.vxt' % (self.prefix, self.radius)
+        assignment_vxt = '%s.assignment.%d.vxt' % (basename, self.radius)
         assignment_vxt = os.path.join(self.prefix, assignment_vxt)
         self.assignment_vxt = assignment_vxt
 
-        catlas_mxt = '%s.catlas.%d.mxt' % (self.prefix, self.radius)
+        catlas_mxt = '%s.catlas.%d.mxt' % (basename, self.radius)
         catlas_mxt = os.path.join(self.prefix, catlas_mxt)
         self.catlas_mxt = catlas_mxt
 
-        original_graph = '%s.gxt' % (self.prefix)
+        original_graph = '%s.gxt' % (basename)
         original_graph = os.path.join(self.prefix, original_graph)
         self.original_graph = original_graph
 
