5ff1373c0b044d722e9f5e96862f29116ff48ea4
Felix Reidl <felix.reidl@gmail.com>
2017-01-25 09:33:46 -0500

    Changed tab to space indentation

--- spacegraphcats/test_domset.py
+++ spacegraphcats/test_domset.py
@@ -8,34 +8,34 @@ import itertools, random
 
 class DomsetTest(unittest.TestCase):
 
-	def test_rdomset(self):
-		n = 100
-		d = 5
-		p = d / n
-		g = Graph.on(list(range(n)))
+    def test_rdomset(self):
+        n = 100
+        d = 5
+        p = d / n
+        g = Graph.on(list(range(n)))
 
-		for x,y in itertools.combinations(range(n),2):
-			if random.random() < p:
-				g.add_edge(x,y)
-		g.remove_loops()
+        for x,y in itertools.combinations(range(n),2):
+            if random.random() < p:
+                g.add_edge(x,y)
+        g.remove_loops()
 
-		# Test dominating set
-		domset, _ = rdomset(g,1)
+        # Test dominating set
+        domset, _ = rdomset(g,1)
 
-		# Test whether domset is indeed a domset
-		for x in g:
-			dominated = x in domset
-			for y in g.neighbours(x):
-				dominated = dominated or y in domset
-			self.assertTrue(dominated)
+        # Test whether domset is indeed a domset
+        for x in g:
+            dominated = x in domset
+            for y in g.neighbours(x):
+                dominated = dominated or y in domset
+            self.assertTrue(dominated)
 
-		# Let's make sure the debug function in rdomset agrees
-		self.assertTrue(verify_domset(g, domset, 1))
+        # Let's make sure the debug function in rdomset agrees
+        self.assertTrue(verify_domset(g, domset, 1))
 
-		# Test higher-radius dominating sets
-		for r in range(2,5):
-			domset, _ = rdomset(g, r)
-			self.assertTrue(verify_domset(g, domset, r))
+        # Test higher-radius dominating sets
+        for r in range(2,5):
+            domset, _ = rdomset(g, r)
+            self.assertTrue(verify_domset(g, domset, r))
 
 if __name__ == '__main__':
     unittest.main()
