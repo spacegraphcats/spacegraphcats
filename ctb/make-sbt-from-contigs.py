8f83c51802823f0fa5dc24202a427f7c11ac4e9f
C. Titus Brown <titus@idyll.org>
2017-04-25 17:23:45 -0700

    upgraded make-sbt-from-contigs.py

---
+++ ctb/make-sbt-from-contigs.py
@@ -0,0 +1,179 @@
+#! /usr/bin/env python
+import os
+import sys
+import argparse
+from sourmash_lib import MinHash
+from collections import defaultdict
+from spacegraphcats.catlas import CAtlas
+import time
+import sourmash_lib
+from sourmash_lib.sbt import SBT, GraphFactory
+from sourmash_lib.sbtmh import search_minhashes, SigLeaf
+from sourmash_lib import signature
+import screed
+
+
+def make_contig_minhashes(contigfile, ksize, scaled):
+    d = {}
+    for record in screed.open(contigfile):
+        mh = MinHash(n=0, ksize=ksize,
+                     max_hash=sourmash_lib.scaled_to_max_hash(scaled))
+        mh.add_sequence(record.sequence)
+        d[int(record.name)] = mh
+
+    return d
+
+
+def load_layer1_to_cdbg(catlas_file, domfile):
+    "Load the mapping between first layer catlas and the original DBG nodes."
+
+    domset = {}
+    for line in open(domfile, 'rt'):
+        dom_node, *beneath = line.strip().split(' ')
+
+        dom_node = int(dom_node)
+        beneath = map(int, beneath)
+
+        domset[dom_node] = set(beneath)
+
+    layer1_to_cdbg = defaultdict(set)
+
+    catlas_to_cdbg = {}
+    for line in open(catlas_file, 'rt'):
+        catlas_node, cdbg_node, level, beneath = line.strip().split(',')
+        if int(level) != 0:
+            continue
+
+        catlas_node = int(catlas_node)
+        cdbg_node = int(cdbg_node)
+        catlas_to_cdbg[catlas_node] = domset[cdbg_node]
+
+    for line in open(catlas_file, 'rt'):
+        catlas_node, cdbg_node, level, beneath = line.strip().split(',')
+        if int(level) != 1:
+            continue
+
+        catlas_node = int(catlas_node)
+        beneath = beneath.split(' ')
+        beneath = list(map(int, beneath))
+
+        for catnode in beneath:
+            dbgnodes = catlas_to_cdbg[catnode]
+            layer1_to_cdbg[catlas_node].update(dbgnodes)
+
+    return layer1_to_cdbg
+
+
+def build_dag(catlas_file, leaf_minhashes, ksize, scaled):
+    catlas_to_cdbg = {}
+
+    x = []
+    for line in open(catlas_file, 'rt'):
+        catlas_node, cdbg_node, level, beneath = line.strip().split(',')
+        if int(level) <= 1:
+            continue
+
+        beneath = beneath.split(' ')
+        beneath = list(map(int, beneath))
+
+        x.append((int(level), int(catlas_node), beneath))
+
+    x.sort()
+    for (level, catlas_node, beneath) in x:
+        merged_mh = MinHash(n=0, ksize=ksize,
+                            max_hash=sourmash_lib.scaled_to_max_hash(scaled))
+
+        for subnode in beneath:
+            mh = leaf_minhashes[subnode]
+            merged_mh.add_many(mh.get_mins())
+        leaf_minhashes[catlas_node] = merged_mh
+
+
+def merge_nodes(child_dict, child_node_list, ksize, scaled):
+    """Merge child nodes into a single minhash."""
+    # merge into a single minhash!
+    merged_mh = MinHash(n=0, ksize=ksize,
+                        max_hash=sourmash_lib.scaled_to_max_hash(scaled))
+
+    for graph_node in child_node_list:
+        if graph_node in child_dict:
+            mh = child_dict[graph_node]
+            merged_mh.merge(mh)
+
+    # add into merged minhashes table.
+    return merged_mh
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('catlas_prefix', help='catlas prefix')
+    p.add_argument('-x', '--bf-size', type=float, default=1e4)
+    p.add_argument('--leaves-only', action='store_true')
+    p.add_argument('--scaled', default=100.0, type=float)
+    p.add_argument('-k', '--ksize', default=31, type=int)
+    p.add_argument('--no-sbt', default=None)
+    args = p.parse_args()
+
+    ksize = args.ksize
+    scaled = args.scaled
+    
+    basename = os.path.basename(args.catlas_prefix)
+    contigfile = '%s.gxt.contigs' % (basename,)
+    contigfile = os.path.join(args.catlas_prefix, contigfile)
+
+    catlas = os.path.join(args.catlas_prefix, args.catlas_prefix + '.catlas')
+    domfile = os.path.join(args.catlas_prefix, args.catlas_prefix + '.domfile')
+    
+    # make minhashes from node contigs
+    print('making contig minhashes...')
+    graph_minhashes = make_contig_minhashes(contigfile, ksize, scaled)
+    print('made {} minhashes'.format(len(graph_minhashes)))
+    print('ksize={} scaled={:.0f}'.format(ksize, scaled))
+
+    # load mapping between dom nodes and cDBG/graph nodes:
+    layer1_to_cdbg = load_layer1_to_cdbg(catlas, domfile)
+    print('loaded {} layer 1 catlas nodes'.format(len(layer1_to_cdbg)))
+    x = set()
+    for v in layer1_to_cdbg.values():
+        x.update(v)
+    print('...corresponding to {} cDBG nodes.'.format(len(x)))
+
+    # create minhashes for catlas leaf nodes.
+    leaf_minhashes = {}
+    for n, (catlas_node, cdbg_nodes) in enumerate(layer1_to_cdbg.items()):
+        mh = merge_nodes(graph_minhashes, cdbg_nodes, ksize, scaled)
+        leaf_minhashes[catlas_node] = mh
+    print('created {} leaf node MinHashes via merging'.format(n + 1))
+
+    if not args.leaves_only:
+        build_dag(catlas, leaf_minhashes, ksize, scaled)
+
+    sigs = []
+    for node_id, mh in leaf_minhashes.items():
+        ss = signature.SourmashSignature('', mh,
+                                         name='node{}'.format(node_id))
+        sigs.append(ss)
+            
+    if args.no_sbt:
+        print('saving sigs to {}'.format(args.no_sbt))
+
+        with open(args.no_sbt, 'wt') as fp:
+            signature.save_signatures(sigs, fp)
+    else:
+        factory = GraphFactory(1, args.bf_size, 4)
+        tree = SBT(factory)
+
+        print('building tree...')
+        for ss in sigs:
+            leaf = SigLeaf(ss.md5sum(), ss)
+            tree.add_node(leaf)
+
+        print('...done with {} minhashes. saving!'.format(len(leaf_minhashes)))
+        sbt_name = os.path.basename(args.catlas_prefix)
+        tree.save(sbt_name)
+        print('saved sbt "{}"'.format(sbt_name))
+
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
