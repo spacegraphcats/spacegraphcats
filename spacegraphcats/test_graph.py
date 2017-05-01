d659054d51e56473c467e7dc42617b70b7f6c3e2
Mike O'Brien <sgsmob@gmail.com>
2017-05-01 17:20:10 -0400

    fraternal pairs testing

--- spacegraphcats/test_graph.py
+++ spacegraphcats/test_graph.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 import unittest
-
+import itertools
 from spacegraphcats.graph import Graph
 
 
@@ -26,6 +26,35 @@ class GraphTest(unittest.TestCase):
         self.assertEquals(graph.in_degree(0), 4)
         self.assertEquals(graph.in_degree(1), 0)
 
+    def test_frat_pairs(self):
+        frat_graph = Graph(num_nodes=9, radius=5)
+        
+        frat_graph.add_arc(1, 0, 1)
+        frat_graph.add_arc(2, 0, 1)
+        frat_graph.add_arc(3, 0, 1)
+        frat_graph.add_arc(4, 0, 1)
+        frat_graph.add_arc(5, 0, 5)
+        frat_graph.add_arc(6, 0, 5)
+        frat_graph.add_arc(7, 0, 5)
+        frat_graph.add_arc(8, 0, 5)
+        # need to make sure we don't include adjacent pairs
+        frat_graph.add_arc(8, 1, 1)
+
+        # arcs of weight 1
+        w1 = [i for i in range(1, 5)]
+        # arcs of weight 5
+        w5 = [i for i in range(5, 9)]
+        # frat pairs of weight 2
+        frat_pairs2 = set(itertools.combinations(w1, 2))
+        # frat pairs of weight 6
+        frat_pairs6 = set(itertools.product(w1, w5))
+        frat_pairs6.remove((1, 8))
+
+        self.assertEquals(set(frat_graph.fraternal_pairs(0, 2)), frat_pairs2)
+        self.assertEquals(set(frat_graph.fraternal_pairs(0, 6)), frat_pairs6)
+
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
