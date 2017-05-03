7dadc56712064b0673189460f52ab1e18f0d589a
C. Titus Brown <titus@idyll.org>
2017-05-02 19:58:05 -0700

    add batch frontier search; fix some downsampling foo

---
+++ search/frontier_search_batch.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+import os
+import sys
+import argparse
+from sourmash_lib import MinHash
+from collections import defaultdict
+from spacegraphcats.catlas import CAtlas
+import time
+import sourmash_lib
+from sourmash_lib.sbt import SBT, GraphFactory
+from sourmash_lib.sbtmh import search_minhashes, SigLeaf
+from sourmash_lib import signature
+import screed
+import pickle
+from typing import List
+
+from .search_catlas_with_minhash import load_dag, load_minhash
+from .frontier_search import frontier_search
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('catlas_prefix', help='catlas prefix')
+    p.add_argument('query_sigs', help='query minhash list', nargs='+')
+    p.add_argument('--overhead', help='\% of overhead', type=float,
+                   default=0.1)
+    args = p.parse_args()
+
+    basename = os.path.basename(args.catlas_prefix)
+    catlas = os.path.join(args.catlas_prefix, basename + '.catlas')
+
+    # load catlas DAG
+    top_node_id, dag, dag_levels = load_dag(catlas)
+    print('loaded {} nodes from catlas {}'.format(len(dag), catlas))
+    minhash_dir = os.path.join(args.catlas_prefix, basename + '.minhashes')
+
+    for filename in args.query_sigs:
+        query_sig = sourmash_lib.signature.load_signatures(filename)
+        query_sig = list(query_sig)[0]
+        print('loaded query sig {}'.format(query_sig.name()), file=sys.stderr)
+        
+        frontier, num_leaves = frontier_search(query_sig, top_node_id, dag, minhash_dir, args.overhead)
+
+        union = load_minhash(frontier.pop(), minhash_dir)
+        for node in frontier:
+            mh = load_minhash(node, minhash_dir)
+            union.merge(mh)
+
+        query_mh = query_sig.minhash.downsample_max_hash(union)
+        union_mh = union.downsample_max_hash(query_sig.minhash)
+
+        containment = query_mh.containment(union_mh)
+        similarity = query_mh.similarity(union_mh)
+
+        print(filename, containment, similarity)
+    
+    
+if __name__ == '__main__':
+    main()
