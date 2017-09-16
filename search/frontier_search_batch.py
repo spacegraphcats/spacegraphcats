82ce898987ac5168e7de1d3065f2feec7e504806
C. Titus Brown <titus@idyll.org>
2017-09-16 08:17:24 -0700

    update batch to save shadow and sig

--- search/frontier_search_batch.py
+++ search/frontier_search_batch.py
@@ -15,9 +15,11 @@ from sourmash_lib.sourmash_args import load_query_signature
 from sourmash_lib.sbt import SBT, GraphFactory
 from sourmash_lib.sbtmh import SigLeaf, search_minhashes
 from typing import List
+import traceback
 
-from .frontier_search import frontier_search, compute_overhead
+from .frontier_search import frontier_search, compute_overhead, find_shadow
 from .search_catlas_with_minhash import load_dag, load_minhash
+from .search_utils import (load_dag, load_layer0_to_cdbg)
 
 
 def main():
@@ -30,15 +32,25 @@ def main():
     p.add_argument('-o', '--output', type=argparse.FileType('w'))
     p.add_argument('-k', '--ksize', default=None, type=int,
                         help='k-mer size (default: 31)')
+    p.add_argument('--savedir', default=None)
     args = p.parse_args()
 
     basename = os.path.basename(args.catlas_prefix)
     catlas = os.path.join(args.catlas_prefix, 'catlas.csv')
+    domfile = os.path.join(args.catlas_prefix, 'first_doms.txt')
 
     # load catlas DAG
-    top_node_id, dag, dag_levels = load_dag(catlas)
+    top_node_id, dag, dag_up, dag_levels = load_dag(catlas)
     print('loaded {} nodes from catlas {}'.format(len(dag), catlas))
     db_path = os.path.join(args.catlas_prefix, 'minhashes.db')
+
+    # load mapping between dom nodes and cDBG/graph nodes:
+    layer0_to_cdbg = load_layer0_to_cdbg(catlas, domfile)
+    print('loaded {} layer 0 catlas nodes'.format(len(layer0_to_cdbg)))
+    x = set()
+    for v in layer0_to_cdbg.values():
+        x.update(v)
+    print('...corresponding to {} cDBG nodes.'.format(len(x)))
 
     w = None
     if args.output:
@@ -50,8 +62,17 @@ def main():
         query_sig = load_query_signature(filename, select_ksize=args.ksize,
                                          select_moltype='DNA')
         print('loaded query sig {}'.format(query_sig.name()), file=sys.stderr)
+
+        try:
+            frontier, _, _, frontier_mh = frontier_search(query_sig, top_node_id, dag, db, args.overhead, True, args.purgatory)
+        except:
+            traceback.print_exc()
+            continue
         
-        frontier, _, _, frontier_mh = frontier_search(query_sig, top_node_id, dag, db, args.overhead, True, args.purgatory)
+        shadow = find_shadow(frontier, dag)
+        cdbg_shadow = set()
+        for x in shadow:
+            cdbg_shadow.update(layer0_to_cdbg.get(x))
 
         query_mh = query_sig.minhash
         query_mh = query_mh.downsample_max_hash(frontier_mh)
@@ -64,7 +85,23 @@ def main():
         print(filename, containment, similarity)
         if args.output:
             w.writerow([filename,containment,similarity,overhead])
-    
+
+        if args.savedir:
+            assert args.output
+            outsig = os.path.basename(filename) + '.frontier'
+            outsig = os.path.join(args.savedir, outsig)
+
+            with open(outsig, 'w') as fp:
+                sig = signature.SourmashSignature('', frontier_mh,
+                                                  name='frontier o={:1.2f} {}'.format(args.overhead, str(args.output)))
+                sourmash_lib.signature.save_signatures([sig], fp)
+
+            outshadow = os.path.basename(filename) + '.shadow'
+            outshadow = os.path.join(args.savedir, outshadow)
+
+            with open(outshadow, 'wb') as fp:
+                pickle.dump(cdbg_shadow, fp)
+                
     
 if __name__ == '__main__':
     main()
