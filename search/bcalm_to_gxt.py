273e4c34ddc7cc2dfde18d8c03c508d034e763e2
Mike O'Brien <mpobrie3@ncsu.edu>
2018-04-25 16:48:21 -0400

    merging thin paths and fixing pendant removal

--- search/bcalm_to_gxt.py
+++ search/bcalm_to_gxt.py
@@ -12,7 +12,26 @@ import collections
 import argparse
 from search.bgzf import bgzf
 
-TRIM_CUTOFF = 1.1
+def find_thin_path(first, second, link_d):
+    """
+    Find the longest thin path whose first two vertices are (first, second.)
+    """
+    path = list()
+    curr = second
+    prev = first
+    while len(link_d[curr]) == 2:
+        path.append(curr)
+        for next_v in link_d[curr]:
+            if next_v != prev:
+                break
+        else:
+            err = "{} has two neighbors, {}, but they are both"
+                " {}".format(curr, link_d[curr], prev)
+            raise ValueError(err)
+        prev = curr
+        curr = next_v
+    return path
+
 
 def main(argv):
     parser = argparse.ArgumentParser()
@@ -23,11 +42,14 @@ def main(argv):
     parser.add_argument('-d', '--debug', action='store_true')
     parser.add_argument('-P', '--pendants', action="store_true",
                         help="don't remove low abundance pendants")
+    parser.add_argument('-a', '--abundance', nargs='?', type=float,
+                        default=1.1)
     args = parser.parse_args(argv)
 
     ksize = args.ksize
 
     trim = not args.pendants
+    trim_cutoff = args.abundance
 
     # track links between contig IDs
     link_d = collections.defaultdict(set)
@@ -74,21 +96,68 @@ def main(argv):
         assert len(abund) == 3
         abund = float(abund[2])
 
+
+        # record the abundance sequence and size
         mean_abunds[contig_id] = abund
-
-        # where are we in the output file?
-        assert contig_id not in offsets
-
         sequences[contig_id] = record.sequence
-
         sizes[contig_id] = len(record.sequence) - ksize + 1
 
     # if we are removing pendants, we need to relabel the contigs so they are
     # consecutive integers starting from 0.  If not, we create dummy data
     # structures to make the interface the same elsewhere in the data
     if trim:
-        non_pendants = [x for x, N in link_d.items() if len(N) > 1 and \
-                        mean_abunds[x] > TRIM_CUTOFF]
+        non_pendants = set(v for v, N in link_d.items() if len(N) > 1 or \
+                        mean_abunds[v] > trim_cutoff)
+        for v, N in link_d.items():
+            if v in non_pendants or len(N) == 0:
+                continue
+            u = N[0]
+            link_d[u].remove(v)
+
+        # pendant removal may have made some vertices with degree 2.  If this creates a long path whose vertices all have degree 2 (a thin path), we should replace the thin path by a single vertex
+        thin_paths = set()  # vertices in a thin path
+        for x in non_pendants:
+            if x in thin_paths or len(link_d[x]) != 2:
+                continue
+            u, v = link_d[x]
+            # start our searches with vertices that have a neighbor of high degree and a neighbor of degree two.  u and v are thus far indistinguishable so we have to try both possibilities (v has high degree vs. u has high degree)
+            if len(link_d[u]) != 2:
+                path = find_thin_path(x, v, link_d)
+            elif len(link_d[v]) != 2:
+                path = find_thin_path(x, u, link_d)
+            else:
+                path = list()
+            thin_paths.update(path)
+            # update sequences
+            for w in path:
+                # decide whether the suffix of w is a prefix of x or vice versa
+                if sequences[w][:ksize-1] == sequences[x][1-ksize:]:
+                    # prefix of w is a suffix of x
+                    sequences[x] += sequences[w][ksize-1:]
+                elif sequences[x][:ksize-1] == sequences[w][1-ksize:]:
+                    # prefix of x is a suffix of w
+                    sequences[x] = sequences[w][:1-ksize] + sequences[x]
+                else:
+                    # there's an error
+                    err = "{} and {} don't have prefix-suffix overlap".format(
+                        sequences[w], sequences[x])
+                    raise ValueError(err)
+                # update sizes and mean_abunds
+                total_abunds = mean_abunds[x] * sizes[x] + \
+                    mean_abunds[w] * (sizes[w] - ksize + 1)
+                sizes[x] += sizes[w] - ksize + 1
+                mean_abunds[x] = total_abunds/sizes[x]
+
+            # update neighbors
+            if len(path) > 0:
+                for y in link_d[w]:
+                    if len(link_d[y]) != 2:
+                        break
+                link_d[y].remove(w)
+                link_d[x].remove(path[0])
+                link_d[x].append(y)
+                link_d[y].append(x)
+        non_pendants -= thin_paths
     else:
         non_pendants = list(link_d.keys())
     aliases = {x: i for i, x in enumerate(non_pendants)}
@@ -107,11 +176,11 @@ def main(argv):
     # write out all of the links, in 'from to' format.
     n_edges = 0
     for node, edgelist in link_d.items():
-        if node not in aliases:
-            continue
+        # if node not in aliases:
+        #     continue
         for next_node in edgelist:
-            if next_node not in aliases:
-                continue
+            # if next_node not in aliases:
+            #     continue
             gxtfp.write('{} {}\n'.format(aliases[node], aliases[next_node]))
             n_edges += 1
 
