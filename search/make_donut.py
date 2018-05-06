f1a8eddf59bd3ed104c9263e92b14e12e396e347
C. Titus Brown <titus@idyll.org>
2018-05-06 06:14:34 -0700

    update make-donut.py to do batch query/subtract, to speed things up:

--- search/make_donut.py
+++ search/make_donut.py
@@ -23,17 +23,28 @@ DEFAULT_THRESHOLD=0.1
 
 def main(argv=sys.argv[1:]):
     p = argparse.ArgumentParser()
-    p.add_argument('sequences')
-    p.add_argument('subtract', nargs='+')
-    p.add_argument('-o', '--output')
+    p.add_argument('--query', nargs='+', action='append')
+    p.add_argument('--subtract', nargs='+', action='append')
+    p.add_argument('-o', '--output-suffix')
     p.add_argument('--threshold', type=float, default=DEFAULT_THRESHOLD)
     p.add_argument('-k', '--ksize', type=int, default=31)
     args = p.parse_args(argv)
 
-    # construct output filename as sequences.donut.fa
-    output = args.output
-    if not output:
-        output = os.path.basename(args.sequences) + '.donut.fa'
+    if not args.query:
+        print('error, must specify at least one query with --query')
+        sys.exit(-1)
+
+    if not args.subtract:
+        print('error, must specify at least one subtract with --subtract')
+        sys.exit(-1)
+
+    args.query = [item for sublist in args.query for item in sublist]
+    args.subtract = [item for sublist in args.subtract for item in sublist]
+
+    # construct output filename as {query}.suffix
+    output_suffix = args.output_suffix
+    if not output_suffix:
+        output_suffix = '.donut.fa'
 
     # load k-mers to subtract
     all_kmers = list()
@@ -49,38 +60,40 @@ def main(argv=sys.argv[1:]):
     table = BBHashTable(all_kmers, fill=1)
     del all_kmers
 
-    # next, iterate over input and do subtract
-    print('subtracting from {} -> {}'.format(args.sequences, output))
-    outfp = open(output, 'wt')
-    n = 0
-    bp = 0
-    n_kept = 0
-    bp_kept = 0
-    for n, record in enumerate(screed.open(args.sequences)):
-        if n % 100000 == 0:
-            print('...', n, n_kept)
+    # next, iterate over each input and do subtract
+    for queryfile in args.query:
+        output = os.path.basename(queryfile) + output_suffix
+        print('subtracting from {} -> {}'.format(queryfile, output))
+        outfp = open(output, 'wt')
+        n = 0
+        bp = 0
+        n_kept = 0
+        bp_kept = 0
+        for n, record in enumerate(screed.open(queryfile)):
+            if n % 100000 == 0:
+                print('...', queryfile, n, n_kept)
 
-        bp += len(record.sequence)
+            bp += len(record.sequence)
 
-        if len(record.sequence) < args.ksize:
-            continue
+            if len(record.sequence) < args.ksize:
+                continue
 
-        kmers = kh.get_kmer_hashes(record.sequence)
+            kmers = kh.get_kmer_hashes(record.sequence)
 
-        present = 0
-        for k in kmers:
-            if table[k]:
-                present += 1
+            present = 0
+            for k in kmers:
+                if table[k]:
+                    present += 1
 
-        f = present / len(kmers)
-        if f < args.threshold:            # keep?
-            outfp.write('>{}\n{}\n'.format(record.name, record.sequence))
-            n_kept += 1
-            bp_kept += len(record.sequence)
+            f = present / len(kmers)
+            if f < args.threshold:            # keep?
+                outfp.write('>{}\n{}\n'.format(record.name, record.sequence))
+                n_kept += 1
+                bp_kept += len(record.sequence)
 
-    print('kept {} ({:.1g} Mbp) of {} ({:.1g} Mbp)'.format(n_kept,
-                                                           bp_kept / 1e6,
-                                                           n, bp / 1e6))
+        print('kept {} ({:.1g} Mbp) of {} ({:.1g} Mbp)'.format(n_kept,
+                                                               bp_kept / 1e6,
+                                                               n, bp / 1e6))
 
     return 0
 
