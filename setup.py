151fbe530427bc09e2c3cb09201942545bf15ae1
C. Titus Brown <titus@idyll.org>
2018-07-31 06:51:14 -0400

    add UC/UW/NCState copyright; update package info a bit

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'spacegraphcats',
+    "description": "tools for biological assembly graph neighborhood analysis",
+    "url": "https://github.com/spacegraphcats/spacegraphcats",
+    "author": "C. Titus Brown, Dominik Moritz, Michael P. O'Brien, Felix Reidl, and Blair D. Sullivan",
+    "author_email": "titus@idyll.org,blair.d.sullivan@gmail.com",
+    "license": "BSD 3-clause",
     packages = find_packages()
 )
