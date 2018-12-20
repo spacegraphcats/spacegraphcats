62a11577d09507e533ad02fb67b02f43c4a5cef7
C. Titus Brown <titus@idyll.org>
2018-12-20 05:57:43 -0800

    update snakemake assist code to be nicer

--- spacegraphcats/snakemake.py
+++ spacegraphcats/snakemake.py
@@ -2,7 +2,7 @@ import os
 import yaml
 
 def catlas_build(conf_file):
-    "Produce the list of files output by 'conf/run <config> build"
+    "Produce the list of files output by 'spacegraphcats <config> build"
     with open(conf_file, 'rt') as fp:
         jj = yaml.load(fp)
 
@@ -13,19 +13,20 @@ def catlas_build(conf_file):
     dirname = '{}_k{}_r{}'.format(catlas_base, ksize, radius)
 
     z = []
-    z.append(catlas_base + '/bcalm.{}.k31.unitigs.fa'.format(catlas_base))
-    z.append(dirname + '/catlas.csv')
-    z.append(dirname + '/cdbg.gxt')
-    z.append(dirname + '/contigs.fa.gz')
-    z.append(dirname + '/contigs.fa.gz.indices')
-    z.append(dirname + '/contigs.fa.gz.info.csv')
-    z.append(dirname + '/contigs.fa.gz.mphf')
-    z.append(dirname + '/first_doms.txt')
+    z.append(os.path.join(catlas_base,
+                          '/bcalm.{}.k31.unitigs.fa'.format(catlas_base)))
+    z.append(os.path.join(dirname, '/catlas.csv'))
+    z.append(os.path.join(dirname, '/cdbg.gxt'))
+    z.append(os.path.join(dirname, '/contigs.fa.gz'))
+    z.append(os.path.join(dirname, '/contigs.fa.gz.indices'))
+    z.append(os.path.join(dirname, '/contigs.fa.gz.info.csv'))
+    z.append(os.path.join(dirname, '/contigs.fa.gz.mphf'))
+    z.append(os.path.join(dirname, '/first_doms.txt'))
     return z
 
 
 def catlas_search(conf_file, cdbg_only=False, suffix=''):
-    "Produce the list of files output by 'conf/run <config> search"
+    "Produce the list of files output by 'spacegraphcats <config> search"
     with open(conf_file, 'rt') as fp:
         jj = yaml.load(fp)
 
@@ -43,18 +44,18 @@ def catlas_search(conf_file, cdbg_only=False, suffix=''):
     z = []
     for x in filenames:
         x = os.path.basename(x)
-        z.append(dirname + '/{}.cdbg_ids.txt.gz'.format(x))
-        z.append(dirname + '/{}.contigs.sig'.format(x))
-        z.append(dirname + '/{}.frontier.txt.gz'.format(x))
-        z.append(dirname + '/{}.response.txt'.format(x))
+        z.append(os.path.join(dirname, '/{}.cdbg_ids.txt.gz'.format(x)))
+        z.append(os.path.join(dirname, '/{}.contigs.sig'.format(x)))
+        z.append(os.path.join(dirname, '/{}.frontier.txt.gz'.format(x)))
+        z.append(os.path.join(dirname, '/{}.response.txt'.format(x)))
 
-    z.append(dirname + '/results.csv')
+    z.append(os.path.join(dirname, '/results.csv'))
 
     return z
 
 
 def catlas_extract(conf_file, cdbg_only=False, suffix=''):
-    "Produce the list of files output by 'conf/run <config> extract_contigs extract_reads"
+    "Produce the list of files output by 'spacegraphcats <config> extract_contigs extract_reads"
     with open(conf_file, 'rt') as fp:
         jj = yaml.load(fp)
 
@@ -72,14 +73,14 @@ def catlas_extract(conf_file, cdbg_only=False, suffix=''):
     z = []
     for x in filenames:
         x = os.path.basename(x)
-        z.append(dirname + '/{}.cdbg_ids.reads.fa.gz'.format(x))
-        z.append(dirname + '/{}.cdbg_ids.contigs.fa.gz'.format(x))
+        z.append(os.path.join(dirname, '/{}.cdbg_ids.reads.fa.gz'.format(x)))
+        z.append(os.path.join(dirname, '/{}.cdbg_ids.contigs.fa.gz'.format(x)))
 
     return z
 
 
 def catlas_search_input(conf_file):
-    "Produce the list of files output by 'conf/run <config> search"
+    "Produce the list of files output by 'spacegraphcats <config> search"
     with open(conf_file, 'rt') as fp:
         jj = yaml.load(fp)
 
