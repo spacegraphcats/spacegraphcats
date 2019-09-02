82ec9fe16cfaf6a514e2a350d6046267f8333f79
C. Titus Brown <titus@idyll.org>
2019-09-01 22:41:37 -0400

    fix bug in test_workflow uncovered by fix in bcalm_to_gxt

--- spacegraphcats/search/test_workflow.py
+++ spacegraphcats/search/test_workflow.py
@@ -56,7 +56,7 @@ def test_dory():
             pass
 
         # convert the bcalm file to gxt
-        args = ['-k', '-21', '-P',
+        args = ['-k', '21', '-P',
                 relative_filename('dory/bcalm.dory.k21.unitigs.fa'),
                 'dory_k21_r1/cdbg.gxt',
                 'dory_k21_r1/contigs.fa.gz']
