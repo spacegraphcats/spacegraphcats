53dc3c22157200ae8dfcd59b4d760c78152c9d2f
C. Titus Brown <titus@idyll.org>
2019-09-07 08:01:00 -0600

    refactor/cleanup test utilities, move to separate location

--- spacegraphcats/search/test_workflow.py
+++ spacegraphcats/search/test_workflow.py
@@ -1,7 +1,9 @@
 import os.path
-import tempfile
 import shutil
 import screed
+
+import spacegraphcats.utils.pytest_utils as pytest_utils
+from spacegraphcats.utils.pytest_utils import (pkg_file, relative_file)
 
 from spacegraphcats.catlas import catlas
 from spacegraphcats.index import index_contigs_by_kmer
@@ -19,159 +21,133 @@ from spacegraphcats.cdbg import index_cdbg_by_minhash
 from spacegraphcats.search import query_by_hashval
 
 
-class TempDirectory(object):
-    def __init__(self):
-        self.tempdir = tempfile.mkdtemp(prefix='sgc_test')
 
-    def __enter__(self):
-        return self.tempdir
+@pytest_utils.in_thisdir
+def test_dory(location):
+    from spacegraphcats.cdbg import bcalm_to_gxt
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        try:
-            shutil.rmtree(self.tempdir, ignore_errors=True)
-        except OSError:
-            pass
+    # make the output directory
+    try:
+        os.mkdir('dory_k21_r1')
+    except FileExistsError:
+        pass
 
-        if exc_type:
-            return False
+    # convert the bcalm file to gxt
+    args = ['-k', '21', '-P',
+            relative_file('dory/bcalm.dory.k21.unitigs.fa'),
+            'dory_k21_r1/cdbg.gxt',
+            'dory_k21_r1/contigs.fa.gz']
 
+    bcalm_to_gxt.main(args)
 
-def relative_filename(filename):
-    thisdir = os.path.dirname(__file__)
-    pkgdir = os.path.join(thisdir, '../..')
-    newpath = os.path.join(pkgdir, filename)
-    return os.path.abspath(newpath)
+    # build catlas
 
+    args = pytest_utils.Args()
+    args.no_checkpoint = True
+    args.level = 0
+    args.radius = 1
+    args.project = 'dory_k21_r1'
 
-class Args(object):
-    pass
+    catlas.main(args)
 
+    # make k-mer search index
+    args = '-k 21 dory_k21_r1'.split()
+    index_contigs_by_kmer.main(args)
 
-def test_dory():
-    with TempDirectory() as location:
-        from spacegraphcats.cdbg import bcalm_to_gxt
+    # do search!!
+    args='dory_k21_r1 dory_k21_r1_search_oh0 --query data/dory-head.fa -k 21'.split()
 
-        # make the output directory
-        try:
-            os.mkdir('dory_k21_r1')
-        except FileExistsError:
-            pass
+    try:
+        query_by_sequence.main(args)
+    except SystemExit as e:
+        assert e.code == 0, str(e)
 
-        # convert the bcalm file to gxt
-        args = ['-k', '21', '-P',
-                relative_filename('dory/bcalm.dory.k21.unitigs.fa'),
-                'dory_k21_r1/cdbg.gxt',
-                'dory_k21_r1/contigs.fa.gz']
+    # check output!
+    output_path = 'dory_k21_r1_search_oh0/'
+    assert os.path.exists(output_path + 'command.txt')
+    assert os.path.exists(output_path + 'dory-head.fa.frontier.txt.gz')
+    assert os.path.exists(output_path + 'dory-head.fa.cdbg_ids.txt.gz')
+    assert os.path.exists(output_path + 'dory-head.fa.response.txt')
+    assert os.path.exists(output_path + 'dory-head.fa.contigs.sig')
+    assert os.path.exists(output_path + 'results.csv')
 
-        bcalm_to_gxt.main(args)
+    with open(output_path + 'results.csv') as fp:
+        lines = fp.readlines()
+        assert len(lines) == 2
 
-        # build catlas
+        last_line = lines[-1].strip()
+        assert last_line == 'data/dory-head.fa,1.0,1.0,1671,2,21,1631,1.0,0.0,0.0'
 
-        args = Args()
-        args.no_checkpoint = True
-        args.level = 0
-        args.radius = 1
-        args.project = 'dory_k21_r1'
+    # run characterize_catlas_regions
+    args = 'dory_k21_r1 dory_k1_r1.vec'.split()
+    characterize_catlas_regions.main(args)
 
-        catlas.main(args)
+    # run extract_unassembled_regions
+    args = 'dory_k21_r1 data/dory-head.fa dory.regions -k 21'.split()
+    extract_unassembled_nodes.main(args)
 
-        # make k-mer search index
-        args = '-k 21 dory_k21_r1'.split()
-        index_contigs_by_kmer.main(args)
+    # run catlas info
+    catlas_info.main(['dory_k21_r1'])
 
-        # do search!!
-        args='dory_k21_r1 dory_k21_r1_search_oh0 --query data/dory-head.fa -k 21'.split()
+    # run extract_contigs
+    args = ['dory_k21_r1',
+            'dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.txt.gz',
+            '-o',
+            'dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.contigs.fa.gz']
+    extract_contigs.main(args)
 
-        try:
-            query_by_sequence.main(args)
-        except SystemExit as e:
-            assert e.code == 0, str(e)
+    assert os.path.exists('dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.contigs.fa.gz')
 
-        # check output!
-        output_path = 'dory_k21_r1_search_oh0/'
-        assert os.path.exists(output_path + 'command.txt')
-        assert os.path.exists(output_path + 'dory-head.fa.frontier.txt.gz')
-        assert os.path.exists(output_path + 'dory-head.fa.cdbg_ids.txt.gz')
-        assert os.path.exists(output_path + 'dory-head.fa.response.txt')
-        assert os.path.exists(output_path + 'dory-head.fa.contigs.sig')
-        assert os.path.exists(output_path + 'results.csv')
+    # run make_bgzf
+    args = ['data/dory-subset.fa', '-o', 'dory/dory.reads.bgz']
+    make_bgzf.main(args)
 
-        with open(output_path + 'results.csv') as fp:
-            lines = fp.readlines()
-            assert len(lines) == 2
+    # run label_cdbg
+    args = ['dory_k21_r1',
+            'dory/dory.reads.bgz', 'dory_k21_r1/reads.bgz.labels']
+    label_cdbg.main(args)
 
-            last_line = lines[-1].strip()
-            assert last_line == 'data/dory-head.fa,1.0,1.0,1671,2,21,1631,1.0,0.0,0.0'
+    # run extract_reads
+    args = ['dory/dory.reads.bgz',
+            'dory_k21_r1/reads.bgz.labels',
+            'dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.txt.gz',
+            '-o',
+            'dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.reads.fa.gz']
+    extract_reads.main(args)
 
-        # run characterize_catlas_regions
-        args = 'dory_k21_r1 dory_k1_r1.vec'.split()
-        characterize_catlas_regions.main(args)
+    # run evaluate_overhead
+    args = ['dory_k21_r1',
+            'data/dory-head.fa',
+            'dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.txt.gz',
+            '-o', 'xyz']
+    evaluate_overhead.main(args)
 
-        # run extract_unassembled_regions
-        args = 'dory_k21_r1 data/dory-head.fa dory.regions -k 21'.split()
-        extract_unassembled_nodes.main(args)
+    # calculate query abundances
+    args = 'dory_k21_r1 data/dory-head.fa -o abundances.csv -k 21'.split()
+    estimate_query_abundance.main(args)
 
-        # run catlas info
-        catlas_info.main(['dory_k21_r1'])
-
-        # run extract_contigs
-        args = ['dory_k21_r1',
-                'dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.txt.gz',
-                '-o',
-                'dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.contigs.fa.gz']
-        extract_contigs.main(args)
-
-        assert os.path.exists('dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.contigs.fa.gz')
-
-        # run make_bgzf
-        args = ['data/dory-subset.fa', '-o', 'dory/dory.reads.bgz']
-        make_bgzf.main(args)
-
-        # run label_cdbg
-        args = ['dory_k21_r1',
-                'dory/dory.reads.bgz', 'dory_k21_r1/reads.bgz.labels']
-        label_cdbg.main(args)
-
-        # run extract_reads
-        args = ['dory/dory.reads.bgz',
-                'dory_k21_r1/reads.bgz.labels',
-                'dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.txt.gz',
-                '-o',
-                'dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.reads.fa.gz']
-        extract_reads.main(args)
-
-        # run evaluate_overhead
-        args = ['dory_k21_r1',
-                'data/dory-head.fa',
-                'dory_k21_r1_search_oh0/dory-head.fa.cdbg_ids.txt.gz',
-                '-o', 'xyz']
-        evaluate_overhead.main(args)
-
-        # calculate query abundances
-        args = 'dory_k21_r1 data/dory-head.fa -o abundances.csv -k 21'.split()
-        estimate_query_abundance.main(args)
-
-        abunds = open('abundances.csv', 'rt').read()
+    abunds = open('abundances.csv', 'rt').read()
 #        assert 'data/dory-head.fa,1.0,1.05' in abunds
 
 
-def test_dory_query_by_hashval():
-    with TempDirectory() as location:
-        shutil.copytree('spacegraphcats/search/test-data/catlas.dory_k21_r1',
-                        os.path.join(location, 'dory_k21_r1'))
-    
-        # index by hashval
-        args = '-k 21 {}/dory_k21_r1/contigs.fa.gz {}/dory_k21_r1_mh.pickle'.format(location, location, location).split()
-        index_cdbg_by_minhash.main(args)
+@pytest_utils.in_tempdir
+def test_dory_query_by_hashval(location):
+    print(os.getcwd())
+    testdata = pkg_file('search/test-data/catlas.dory_k21_r1')
+    shutil.copytree(testdata, './dory_k21_r1')
 
-        # query by hashval
-        with open(os.path.join(location, 'xxx.list'), 'wt') as fp:
-            fp.write("""1432815083088457
+    # index by hashval
+    args = '-k 21 dory_k21_r1/contigs.fa.gz dory_k21_r1_mh.pickle'
+    index_cdbg_by_minhash.main(args.split())
+
+    # query by hashval
+    with open('xxx.list', 'wt') as fp:
+        fp.write("""1432815083088457
 939339108487323
 660775515984191
 1056719064763796
 629309120813421
 1780496337566254
 """)
-        args = '-k 21 {}/dory_k21_r1 {}/dory_k21_r1_mh.pickle {}/xxx.list {}/xxx.list.dir'.format(*[location]*4).split()
-        query_by_hashval.main(args)
+    args = '-k 21 dory_k21_r1 dory_k21_r1_mh.pickle xxx.list xxx.list.dir'
+    query_by_hashval.main(args.split())
