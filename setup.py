3e8cf2ebe632f134ddd0b32052993a980b82b098
C. Titus Brown <titus@idyll.org>
2020-10-31 16:59:37 -0700

    add long_description

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+   long_description = f.read()
+
 CLASSIFIERS = [
     "Environment :: Console",
     "Environment :: MacOS X",
@@ -46,4 +52,6 @@ setup(
         "khmer",
         "bbhash >= 0.5",
     ],
+   long_description=long_description,
+   long_description_content_type="text/markdown",
 )
